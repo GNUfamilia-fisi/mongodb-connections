bb83f911bf8179d0a684f22877510c30e20b5235
Paolo Flores <pflores.fisi22@gmail.com>
2023-06-29 19:44:13 -0500

    fix(nodejs): fix cors errors

--- nodejs/src/main.js
+++ nodejs/src/main.js
@@ -5,10 +5,9 @@ import {
 } from 'mongodb'
 
 import {createServer} from 'node:http'
-import {stringify} from 'node:querystring'
- 
+
 //NOTE: this place has to handle all client requests
- 
+
 const queryEditable = class {
     constructor(title, description, thumbnail) {
         this.title = title,
@@ -42,7 +41,7 @@ const updateFromDatabase = async (filter, newValues) => {
 const Create = (request, response) => {
     let body = []
     let newVideo = new dataTemplate()
-    
+
     request
     .on("data", (chunk) => {
         body.push(chunk)
@@ -58,56 +57,64 @@ const Create = (request, response) => {
         insertAtDatabase(JSON.parse(newBody)).then(e => {
             console.log("hola papu")
             newVideo._id = e.insertedId.toString()
-            response.end(JSON.stringify(newVideo))
 
-            response.setHeader("Content-type", "application/json")
-            response.setHeader("Access-Control-Allow-Origin", "*")
-            response.writeHead(200)
+            response.writeHead(200, {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+                "ngrok-skip-browser-warning": "true",
+                "Content-Type": "application/json"
+            });
+            response.end(JSON.stringify(newVideo));
         })
-    
-        console.log(newVideo)
     })
 }
 
 const Read = (response, id) => {
     let list = []
-    
+
     console.log(id)
     run().then(e => {
         let newVideo = new dataTemplate()
-        
+
         if (id) {
             e.forEach((elem) => {
                 newVideo.title = elem.title,
                 newVideo.description = elem.description,
                 newVideo.thumbnail = elem.thumbnail
                 newVideo._id = elem._id
-                
+
                 if (id[1] === elem._id.toString()) {
                     list = elem
                 }
             })
         }
-        
+
         else {
             e.forEach((elem) => {
                 newVideo.title = elem.title,
                 newVideo.description = elem.description,
                 newVideo.thumbnail = elem.thumbnail
                 newVideo._id = elem._id
-                
+
                 list.push(elem)
             })
         }
-        
+
         console.log("hola papu")
-        
-        response.setHeader("Content-type", "application/json")
-        response.setHeader("Access-Control-Allow-Origin", "*")
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-        response.setHeader("ngrok-skip-browser-warning", "true")
-        response.writeHead(200)
+
+        // response.setHeader("Content-type", "application/json")
+        // response.setHeader("Access-Control-Allow-Origin", "*")
+        // response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        // response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+        // response.setHeader()
+        response.writeHead(200, {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+            "ngrok-skip-browser-warning": "true",
+            "Content-Type": "application/json"
+        })
         response.end(JSON.stringify(list))
     })
 }
@@ -133,11 +140,17 @@ const Update = (request, response, id) => {
             console.log(e)
         })
     })
-    response.setHeader("Access-Control-Allow-Origin", "*")
-    response.end("hola")
+    response.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        "ngrok-skip-browser-warning": "true",
+        "Content-Type": "application/json"
+    });
+    response.end();
 }
 
-const Delete = (response, id) => {
+const Delete = async (response, id) => {
     if (!id) throw new Error("'id' field cannot be undefined")
     const query = {
         _id: new ObjectId(id[1])
@@ -145,18 +158,23 @@ const Delete = (response, id) => {
 
     console.log(query)
 
-    deletefromDatabase(query).then(e => {
-        console.log(e)
-        response.setHeader("Access-Control-Allow-Origin", "*")
-        response.end("hola")
-    })
+    const delete_result = await deletefromDatabase(query);
+    console.log({ delete_result });
+    response.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        "ngrok-skip-browser-warning": "true",
+        "Content-Type": "application/json"
+    });
+    response.end();
 }
 
 const mainHandler = (request, response) => {
     if (request.method === "GET") {
         if (request.url === "/api/videos") {
             Read(response)
-        } 
+        }
         if (/\/api\/videos\/(\w+)/.test(request.url)) {
             Read(response, /\/api\/videos\/(\w+)/.exec(request.url))
         }
@@ -177,18 +195,23 @@ const mainHandler = (request, response) => {
         }
     }
     else if (request.method === "OPTIONS") {
-        response.writeHead(200);
-    } 
+        response.writeHead(200, {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        });
+        response.end();
+    }
 }
 
 const port = 8080
 const server = createServer(mainHandler);
 server.listen(port, "localhost", () => {
     console.log(`Running at ${port} fyi`)
-}) 
- 
+})
+
 const uri = process.env.MONGO_URI
- 
+
 let mongoClient = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -209,9 +232,8 @@ const run = async () => {
 
     const chosenCollection = "hola"
     let collection = DB.collection(chosenCollection).find({})
-    
+
     return (await collection.toArray())
 }
 
 run().catch(console.log)
-
